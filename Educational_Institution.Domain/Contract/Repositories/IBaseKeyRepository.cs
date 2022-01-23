8be6d770613d02a91d9628dfa0ca11e0c54f776a
Morteza Sarang <mortezasarang78@yahoo.com>
2022-01-23 19:50:03 +0330

    Add Repositories

---
+++ Educational_Institution.Domain/Contract/Repositories/IBaseKeyRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Educational_Institution.Domain.Contract.Repositories
+{
+    public interface IBaseKeyRepository<TEntity,TKey>
+    {
+        TEntity Get(TKey key);
+        TEntity Find(TKey key);
+        void Delete(TKey key);
+
+    }
+}
---
+++ Educational_Institution.Domain/Contract/Repositories/IBaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Educational_Institution.Domain.Contract;
+
+namespace Educational_Institution.Domain.Contract.Repositories
+{
+    public interface IBaseRepository<TEntity>
+    {
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        IEnumerable<TEntity> Get(Expression<Func<TEntity,bool>> expression);
+        IEnumerable<TEntity> GetAll();
+
+    }
+}
