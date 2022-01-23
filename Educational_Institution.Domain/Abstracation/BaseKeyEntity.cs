a93a3cbb9bac2f72e8c7da012751efef2090765e
Morteza Sarang <mortezasarang78@yahoo.com>
2022-01-23 19:49:43 +0330

    Add Abstracation

---
+++ Educational_Institution.Domain/Abstracation/BaseKeyEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Educational_Institution.Domain.Contract.Entities;
+
+namespace Educational_Institution.Domain.Abstracation
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T">Type Of Key</typeparam>
+    public abstract class BaseKeyEntity<T> : BaseEntity, IBaseKeyEntity<T>
+    {
+        public T Key { get ; set ; }
+    }
+}
---
+++ Educational_Institution.Domain/Abstracation/BaseKeyValueEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Educational_Institution.Domain.Abstracation
+{
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <typeparam name="T">Type Of Key</typeparam>
+  /// <typeparam name="TValue">Type Of Value</typeparam>
+    public abstract class BaseKeyValueEntity<T,TValue>:BaseKeyEntity<T>
+    {
+        public TValue Value { get; set; }
+    }
+}
