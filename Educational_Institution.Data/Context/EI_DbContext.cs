1bdcb841d79592d5cb6422f9a954b1f68dd6cf98
Morteza Sarang <mortezasarang78@yahoo.com>
2022-01-24 12:08:52 +0330

    Add Context

---
+++ Educational_Institution.Data/Context/EI_DbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Educational_Institution.Domain.Models.IdentityModel;
+using Educational_Institution.Domain.Models.Entities;
+
+namespace Educational_Institution.Data.Context
+{
+    public class EI_DbContext: IdentityDbContext<User,Role,int>
+    {
+        public EI_DbContext(DbContextOptions<EI_DbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Attr> Attrs { get; set; }
+        public DbSet<AttrValue> AttrValues { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Grad> Grads { get; set; }
+
+    }
+}
